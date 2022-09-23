0262c4a59c92007523ec05654a4c37c278a00be8
aledguedes <78328104+aledguedes@users.noreply.github.com>
2022-09-23 13:15:23 -0300

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,16 +15,33 @@ export class AppComponent {
     { id: 5, name: 'Bahia' },
     { id: 6, name: 'Ceará' },
   ];
-  listRodadas: any = [];
+  firstTurn: any = [];
+  secondTurn: any = [];
   classificacao = {};
 
   gerar() {
     // const time: any[] = this.times;
-    const time: any = ['sp', 'rj', 'mg', 'ba', 'ms', 'mt'];
+    const time: any = [
+      'Sãu Paulo',
+      'Santos',
+      'Corinthians',
+      'Palmeiras',
+      'Flamengo',
+      'Fluminense',
+      'Vasco',
+      'Botafogo',
+    ];
     const lengthArray = time.length;
-    const second = time.splice(lengthArray / 2, Number.MAX_VALUE);
+    const time02 = time.splice(lengthArray / 2, Number.MAX_VALUE);
+    const time01 = time;
+    // console.log('ORIGINAL', time01, time02);
+    this.primeiroTruno(time01, time02, lengthArray);
+    this.segundoTurno(time02, time01, lengthArray);
+  }
+
+  primeiroTruno(timeA, timeB, lengthArray) {
+    // console.log('PRIMEIRO TURNO', timeA, timeB, lengthArray);
     const round: any = [];
-    console.log('ORIGINAL', time, second);
     let mandator: any;
     let visitor: any;
     let i = 0;
@@ -39,8 +56,8 @@ export class AppComponent {
         jogos: [],
       };
       for (let j = 0; j < lengthArray / 2; j++) {
-        mandator = time[j];
-        visitor = second[j];
+        mandator = timeA[j];
+        visitor = timeB[j];
 
         round[i].jogos.push({
           mandator,
@@ -48,13 +65,48 @@ export class AppComponent {
         });
       }
       k++;
-      const back1 = second.splice(0, 1); // BA
-      const back2 = time.splice(2, 1); //MG
-      time.splice(1, 0, back1[0]);
-      second.splice(2, 0, back2[0]);
-      this.listRodadas = round;
+      const back1 = timeB.splice(0, 1); // BA
+      const back2 = timeA.splice(2, 1); //MG
+      timeA.splice(1, 0, back1[0]);
+      timeB.splice(2, 0, back2[0]);
     }
+    console.log('RESULT PRIMEIRO TURNO', round);
+    this.firstTurn = round;
+    // return round;
+  }
 
-    console.log('his.listRodadas', this.listRodadas);
+  segundoTurno(timeA, timeB, lengthArray) {
+    const round: any = [];
+    let mandator: any;
+    let visitor: any;
+    let i = 0;
+    let k = 1;
+
+    for (i = 0; i < lengthArray - 1; i++) {
+      let rodada = k,
+        dt_partida = new Date();
+      round[i] = {
+        rodada,
+        dt_partida,
+        jogos: [],
+      };
+      for (let j = 0; j < lengthArray / 2; j++) {
+        mandator = timeA[j];
+        visitor = timeB[j];
+
+        round[i].jogos.push({
+          mandator,
+          visitor,
+        });
+      }
+      k++;
+      const back1 = timeB.splice(0, 1); // BA
+      const back2 = timeA.splice(2, 1); //MG
+      timeA.splice(1, 0, back1[0]);
+      timeB.splice(2, 0, back2[0]);
+    }
+    console.log('RESULT SEGUNDO TURNO', round);
+    this.secondTurn = round;
+    // return round;
   }
 }
